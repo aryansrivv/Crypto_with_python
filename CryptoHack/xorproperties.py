c6c9fc8964dc22718da506a7e847fa01529b4d89
Aryan Srivastava <aryansrivastava103@gmail.com>
2022-07-11 08:13:54 +0530

    Update xorproperties.py

--- CryptoHack/xorproperties.py
+++ CryptoHack/xorproperties.py
@@ -2,7 +2,7 @@
 # just xor them you get xor of key 1 ,2 ,3 
 # now xor it with the given flag^k1^k2^k3 you get flag^0 = flag 
 
-
+from pwn import xor 
 key1 = 'a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313'
 key1_by = bytes.fromhex(key1)
 key2 = 'c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1'
